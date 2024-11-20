e50ea517b67b1d496cc8b4a8ee2feedd9fd6bee0
basha105 <brentbasha17@gmail.com>
2024-11-20 15:43:48 -0800

    Added photo for testing purposes

---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Life() {
+    return [<h2>Hey Norah. How was school today.</h2>,
+    <img src="./public/cris.png" alt="" />
+    ]
+}
+        
+
+
+export default Life;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Life from './Greeting.jsx';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Life />
   </StrictMode>,
 )
