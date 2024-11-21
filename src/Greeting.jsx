926761061b72348fe2f2d089158d49d70534afaa
basha105 <brentbasha17@gmail.com>
2024-11-21 11:17:24 -0800

    Can finally run npm run dev

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -3,13 +3,12 @@ function Life() {
         <div>
             <h1>Test Title</h1>
             <svg>
-                <circle cx="25" cy="75" r="20" stroke="green" stroke-width="2"></circle>
+                <circle cx="25" cy="75" r="20" stroke="blue" stroke-width="1"></circle>
             </svg>
 
             <form>
                 <input type="text"></input>
             </form>
-
         </div>
     )
     
