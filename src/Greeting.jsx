74d138a93747625da8f853a02a04a772a3931c2f
basha105 <brentbasha17@gmail.com>
2024-12-02 20:22:05 -0800

    Fix

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,21 +1,39 @@
-import './App.css'
+import { Component } from 'react';
 
-function Button({text = "Click me", color = "blue", fontSize = 12 }) {
-    const buttonStyle = {
-        color: color,
-        fontSize: fontSize + 'px'
-    };
-    return <button style={buttonStyle}>{text}</button>;
+class ClassInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            todos: [],
+            inputVal: "",
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        handleInputChange(e) {
+            this.setState((state) => {
+                inputVal: e.target.value;
+            })
+        }
+    }
+
+    render() {
+        return (
+            <section>
+                <h3>Head</h3>
+                <form>
+                    <label htmlFor="task-entry">Enter a task: </label>
+                    <input type="text" name="task-entry"/>
+                    <button type="submit">Submit</button>
+                </form>
+                <h4>All the tasks!</h4>
+                <ul></ul>
+            </section>
+        )
+    }
+
 }
 
-function CreateButtons() {
-    return (
-        <div>
-            <Button text="Ion" fontSize = "18"/>
-            <Button text="Brent" color = "green" fontSize ="18" />
-            <Button text="norah" color = "pink" fontSize = "18"/>
-        </div>
-    );
-}
-
-export default CreateButtons
+export default ClassInput
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import CreateButtons from './Greeting.jsx';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CreateButtons />
+  
   </StrictMode>,
 )
